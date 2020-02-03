343956c6fa1b86d7869126e202de0f236b092455
Jonathan Ben Avraham <jbenav200@caledonian.ac.uk>
2020-02-03 18:05:16 +0000

    changed models to support new API

--- registry/models.py
+++ registry/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 # Create your models here.
-class NEO(models.Model):
-    id = models.IntegerField()
-    name = models.CharField(max_length=200)
-
-
-class Asteroid(NEO):
-    diameter = models.FloatField()
-    orbit_circumference = models.FloatField()
+class Meteorite(models.Model):
+    id = models.PositiveIntegerField()
+    name = models.CharField(max_length=50)
+    rec_class = models.CharField(max_length=50)
+    mass = models.FloatField()
+    fall = models.CharField(max_length=20)
+    year = models.DateTimeField()
+    rec_latitude = models.CharField(max_length=200)
+    rec_longitude = models.CharField(max_length=200)
+    geolocation = models.CharField(max_length=250)
 
