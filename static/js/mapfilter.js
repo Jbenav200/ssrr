de9adde1376db943df87b0996be09869ad5106f7
Jonathan Ben Avraham <jbenav200@caledonian.ac.uk>
2020-03-13 16:21:58 +0000

    added functionality to the map filter button, sidebar now opens and closes on click

---
+++ static/js/mapfilter.js
@@ -0,0 +1,14 @@
+
+// this function opens the sidebar for the map filter
+
+function openSidebarFilter(){
+    $(".fas").css('visibility', 'hidden');
+    $('#filter-sidebar').css('visibility', 'visible');
+}
+
+//function closes the sidebar for the map filter
+
+function closeSidebarFilter(){
+    $('.fas').css('visibility', 'visible');
+    $('#filter-sidebar').css('visibility', 'hidden');
+}
